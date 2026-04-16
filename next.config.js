b575d20221c09aaca61e7cad19b1d9469780be0b
ramizkrdnz <ramiz.karadeniz81@gmail.com>
2026-04-16 20:45:56 +0200

    vercel settings

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const basePath = process.env.NEXT_PUBLIC_BASE_PATH || ''
+const isVercel = process.env.VERCEL === '1'
 
 const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
+  ...(isVercel ? {} : { output: 'export', trailingSlash: true }),
   basePath,
   assetPrefix: basePath || undefined,
   images: {
